02643dbacd2cbfa731e210b65404afb3c07a9aff
Yz4230 <yuzupon1480@gmail.com>
2021-04-08 18:27:14 +0900

    Setup react eslint prettier with typescript

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react-hooks", "react", "@typescript-eslint"],
+  rules: {
+    "react/prop-types": "off",
+  },
+};
