37990ff1e4f665b716248ac9b4c8aa0d51a2e844
Yz4230 <yuzupon1480@gmail.com>
2021-04-10 09:59:34 +0900

    Fix: cannot read environment variables

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,13 +3,13 @@ import "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FBASE_API_KEY,
-  authDomain: process.env.FBASE_AUTH_DOMAIN,
-  projectId: process.env.FBASE_PROJECT_ID,
-  storageBucket: process.env.FBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FBASE_APP_ID,
-  measurementId: process.env.FBASE_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_FBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
