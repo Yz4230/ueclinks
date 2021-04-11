6532c0a629d0ccb8473d41a7f601094f818f33c1
Yz4230 <yuzupon1480@gmail.com>
2021-04-12 07:07:54 +0900

    Improve: using bigger anchor for smartphones

--- src/components/LinkCard.tsx
+++ src/components/LinkCard.tsx
@@ -12,43 +12,51 @@ const LinkCard: React.FC<Props> = ({ link }) => {
 
   return (
     <div className="rounded-md md:rounded-none shadow md:shadow-none p-2">
-      <div className="flex">
+      <div className="relative">
         <a
-          className="truncate"
+          className="w-full h-full block absolute md:hidden"
           href={link.href}
           target="_blank"
           rel="noreferrer"
-          onMouseOver={cursorOvered}
-          onMouseOut={cursorOuted}
-        >
-          <span className="text-sm text-green-700">{link.href}</span>
-        </a>
-      </div>
-      <div className="flex">
-        <a
-          className="flex items-center"
-          href={link.href}
-          target="_blank"
-          rel="noreferrer"
-          onMouseOver={cursorOvered}
-          onMouseOut={cursorOuted}
-        >
-          <h3 className="font-semibold text-xl text-indigo-600 cursor-pointer">
-            <span className={underlined ? "underline" : ""}>{link.title}</span>
-          </h3>
-          {link.onCampusOnly && (
-            <div className="inline-block">
-              <div className="bg-red-500 border-2 border-red-600 text-white font-bold flex items-center rounded-full text-xs px-2 ml-1">
-                学内のみ
+        />
+        <div className="flex">
+          <a
+            className="truncate"
+            href={link.href}
+            target="_blank"
+            rel="noreferrer"
+            onMouseOver={cursorOvered}
+            onMouseOut={cursorOuted}
+          >
+            <span className="text-sm text-green-700">{link.href}</span>
+          </a>
+        </div>
+        <div className="flex">
+          <a
+            className="flex items-center"
+            href={link.href}
+            target="_blank"
+            rel="noreferrer"
+            onMouseOver={cursorOvered}
+            onMouseOut={cursorOuted}
+          >
+            <h3 className="font-semibold text-xl text-indigo-600 cursor-pointer">
+              <span className={underlined ? "underline" : ""}>
+                {link.title}
+              </span>
+            </h3>
+            {link.onCampusOnly && (
+              <div className="inline-block">
+                <div className="bg-red-500 border-2 border-red-600 text-white font-bold flex items-center rounded-full text-xs px-2 ml-1">
+                  学内のみ
+                </div>
               </div>
-            </div>
-          )}
-        </a>
-      </div>
-      <div className="ml-1">
-        <div className="text-gray-500">{link.description}</div>
-      </div>
-      <div>
+            )}
+          </a>
+        </div>
+        <div className="ml-1">
+          <div className="text-gray-500">{link.description}</div>
+        </div>
         <span className="text-sm text-gray-700">キーワード: </span>
         {link.keywords.map((k) => (
           <div
